1d9f0dc0adf41b979214d17da2875cc15b5a0ead
agarwalv123 <vartika.mca@gmail.com>
2020-06-10 11:07:15 +0800

    login testcase

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>80bc92cd-f632-44f1-aab9-abd00961033c</testSuiteGuid>
+   <testCaseLink>
+      <guid>feda981e-6e4b-41d0-8832-0f587cfb5657</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
